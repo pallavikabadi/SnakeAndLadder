e472b3c1230f79f0caf762d9ef650d6fc272adfc
pallavikabadi <58931162+pallavikabadi@users.noreply.github.com>
2023-05-02 07:49:11 +0530

    Added:Roll The Die Using Random Function

--- src/com/bridgelabz/SnakeLadder.java
+++ src/com/bridgelabz/SnakeLadder.java
@@ -2,9 +2,17 @@ package com.bridgelabz;
 
 public class SnakeLadder {
     public static int position=0;
+    
+    public static int rollDie(){
+        int randomCheck=(int)(Math.random()*6);
+
+        return randomCheck+1;
+    }
 
     public static void main(String[] args) {
         System.out.println("Welcome to Snake & Ladder");
-        System.out.println("Player : 웃 at " +position+" position");
+        System.out.println("Player : 웃 at " +position+" position");         
+        System.out.println("Got number after player rolls the die:"+rollDie());
+
     }
 }
