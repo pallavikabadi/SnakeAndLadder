23f0186afe047449c36ef86562740aec19144211
pallavikabadi <58931162+pallavikabadi@users.noreply.github.com>
2023-05-02 08:02:06 +0530

    Added:PlayerReachesTheWinning Position 100

--- src/com/bridgelabz/SnakeLadder.java
+++ src/com/bridgelabz/SnakeLadder.java
@@ -1,23 +1,25 @@
 package com.bridgelabz;
 
 public class SnakeLadder {
-    public enum option{NO_PLAY,SNAKE,LADDER}
-    final static int NO_PLAY =1;
-    final static int SNAKE =2;
-    final static int LADDER =3;
+    //public enum option{NO_PLAY,SNAKE,LADDER}
+    static int INITIAL_POSITION = 0;
+    static final int WINING_POSITION = 100;
+    final static int NO_PLAY = 1;
+    final static int SNAKE = 2;
+    final static int LADDER = 3;
 
-    public static int position=0;
+    public static int position = 0;
 
-    public static int checkOption()
-    {
-        int random=(int)(Math.random()*10)%3+1;
+    public static int checkOption() {
+        int random = (int) (Math.random() * 10) % 3 + 1;
         return random;
     }
-    public static int rollDie()
-    {
-        int randomCheck= (int) (Math.random()*6);
-        return randomCheck+1;
+
+    public static int rollDie() {
+        int randomCheck = (int) (Math.random() * 6);
+        return randomCheck + 1;
     }
+
     public static void main(String[] args) {
         System.out.println("Welcome to Snake & Ladder");
         System.out.println("Player : at " + position + " position");
@@ -25,17 +27,19 @@ public class SnakeLadder {
         System.out.println("Got number after  player rolls the die:" + dieNum);
         position = dieNum + position;
         System.out.println("Check the position :" + position);
-        int option=checkOption();
+        int option = checkOption();
 
-        switch (option){
-            case SNAKE:
-                position -=dieNum;
-                break;
-            case LADDER:
-                position +=dieNum;
-                break;
-            case NO_PLAY:
-                break;
+        while (position < WINING_POSITION) {
+            switch (option) {
+                case SNAKE:
+                    position -= dieNum;
+                    break;
+                case LADDER:
+                    position += dieNum;
+                    break;
+                case NO_PLAY:
+                    break;
+            }
         }
     }
 }
