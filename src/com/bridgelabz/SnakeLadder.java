c8186ca3eb401831a223da9661cd0b7fd1c320cf
pallavi kabadi <pallavi08jadhav@gmail.com>
2023-05-02 07:00:30 +0530

    Added:Snake And Ladder

---
+++ src/com/bridgelabz/SnakeLadder.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+public class SnakeLadder {
+    public static int position=0;
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Snake & Ladder");
+        System.out.println("Player : 웃 at " +position+" position");
+    }
+}
